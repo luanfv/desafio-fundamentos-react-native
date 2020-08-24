0c5c828471386019dcb31c90ad280c52dd8a4fcf
Luan <luanfrancavieira@gmail.com>
2020-08-24 19:20:05 -0300

    finish test

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -51,12 +51,32 @@ const CartProvider: React.FC = ({ children }) => {
   }, [products]);
 
   const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    const newProducts = products.map(product => product.id === id 
+      ? { ...product, quantity: product.quantity + 1 } 
+      : product
+    );
+
+    setProducts(newProducts);
+    await AsyncStorage.setItem('@GoMarketplace:carts', JSON.stringify(newProducts));
+  }, [products]);
 
   const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    const newProducts: Product[] = [];
+
+    products.map(product => {
+      if (product.id === id) {
+        if (product.quantity > 1)
+          newProducts.push({ ...product, quantity: product.quantity - 1 });
+
+        return
+      }
+
+      newProducts.push(product);
+    });
+
+    setProducts(newProducts);
+    await AsyncStorage.setItem('@GoMarketplace:carts', JSON.stringify(newProducts));
+  }, [products]);
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
